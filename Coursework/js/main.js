b25543139ba799d5845de67df2d1f69de206507a
Juho <juke.koski@gmail.com>
2025-05-22 15:31:01 +0300

    coding done for: FAQ elements [1:12:20].

---
+++ Coursework/js/main.js
@@ -0,0 +1,41 @@
+// FAQ Accordion
+
+document.addEventListener('DOMContentLoaded', function () {
+    const faqContainer = document.querySelector('.faq-content');
+
+    faqContainer.addEventListener('click', function (e) {
+        /*console.log(e.target); */
+        const groupHeader = e.target.closest('.faq-group-header');
+        
+        if (!groupHeader) return;
+        /*console.log(groupHeader); */
+
+        const group = groupHeader.parentElement;
+        const groupBody = group.querySelector('.faq-group-body');
+        const icon = groupHeader.querySelector('i');
+
+        // toggle icon + -
+        icon.classList.toggle('fa-plus');
+        icon.classList.toggle('fa-minus');
+
+        // Toggle visibility of body
+        groupBody.classList.toggle('open');
+
+        // close other open FAQ bodies
+        const otheGroups = faqContainer.querySelectorAll('.faq-group');
+
+        otheGroups.forEach((otherGroup) => {
+            if (otherGroup != group) {
+                const otherGroupBody = otherGroup.querySelector('.faq-group-body');
+                const otherIcon = otherGroup.querySelector('.faq-group-header i');
+            
+                otherGroupBody.classList.remove('open');
+                otherIcon.classList.remove('fa-minus');
+                otherIcon.classList.add('fa-plus');
+
+            }
+
+        });
+
+    });
+});
