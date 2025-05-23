6d35a798fc6f3c93b3c399e668db6a7affcda5a3
Juho <juke.koski@gmail.com>
2025-05-23 14:30:32 +0300

    coding done for: Mobile Menu & Responsiveness [1:47:05].

--- Coursework/js/main.js
+++ Coursework/js/main.js
@@ -39,3 +39,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
     });
 });
+
+// Mobile Menu
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburgerButton = document.querySelector('.hamburger-button');
+  const mobileMenu = document.querySelector('.mobile-menu');
+
+  hamburgerButton.addEventListener('click', () =>
+    mobileMenu.classList.toggle('active')
+  );
+});
