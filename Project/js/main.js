f180ea79f67a43b81e15fe35072fade5c20fe0dc
Juho <juke.koski@gmail.com>
2025-06-06 19:25:44 +0300

    Finished rapid and blitz sections and added new copyright free image to home screen.

--- Project/js/main.js
+++ Project/js/main.js
@@ -1,22 +1,38 @@
+
+// Crown icon in Rapid section
+document.addEventListener('DOMContentLoaded', function () {
+    const faqContainer = document.querySelector('.crown');
+
+    faqContainer.addEventListener('click', function (e) {
+        const groupHeader = e.target.closest('.crown-header');
+        
+        if (!groupHeader) return;
+
+
+        const group = groupHeader.parentElement;
+        const groupBody = group.querySelector('.crown-text');
+        const icon = groupHeader.querySelector('i');
+
+        groupBody.classList.toggle('open');
+
+
+    });
+});
+
+// About Us text
 document.addEventListener('DOMContentLoaded', function () {
     const faqContainer = document.querySelector('.content');
 
     faqContainer.addEventListener('click', function (e) {
-        /*console.log(e.target);*/
         const groupHeader = e.target.closest('.about-us-group-header');
         
         if (!groupHeader) return;
-        /*console.log(groupHeader); */
+
 
         const group = groupHeader.parentElement;
         const groupBody = group.querySelector('.about-us-group-body');
         const icon = groupHeader.querySelector('i');
 
-        // toggle icon + -
-            //icon.classList.toggle('fa-plus');
-            //icon.classList.toggle('fa-minus');
-
-        // Toggle visibility of body
         groupBody.classList.toggle('open');
 
 
